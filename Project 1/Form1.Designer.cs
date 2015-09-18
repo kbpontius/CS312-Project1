38e9940e28b7f30a41d1e7e15c8d7c305f5d4b7b
Kyle Pontius <kbpontius@gmail.com>
2015-09-17 23:03:45 -0600

    project commit

---
+++ Project 1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Project_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKValue = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtKValue
+            // 
+            this.txtKValue.Location = new System.Drawing.Point(12, 33);
+            this.txtKValue.Name = "txtKValue";
+            this.txtKValue.Size = new System.Drawing.Size(260, 20);
+            this.txtKValue.TabIndex = 0;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 91);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(260, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 149);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(260, 20);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "K-Value";
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.Location = new System.Drawing.Point(12, 242);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(75, 23);
+            this.btnSolve.TabIndex = 2;
+            this.btnSolve.Text = "Solve";
+            this.btnSolve.UseVisualStyleBackColor = true;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 284);
+            this.Controls.Add(this.btnSolve);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtKValue);
+            this.Name = "Form1";
+            this.Text = "Project 1 - Fermat Primality Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKValue;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Project 1/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+    ALL SPACE AND TIME COMPLEXITY EVALUATIONS
+    WERE INCLUDED IN THE SUBMISSION FILE, BY METHOD.
+    PLEASE REFER TO THAT FILE FOR THE ASSOCIATED
+    BIG-O NUMBERS AND EXPLANATIONS.
+*/
+
+namespace Project_1
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        HashSet<int> set = new HashSet<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSolve_Click(object sender, EventArgs e)
+        {
+            bool result = false;
+
+            if (txtInput.Text.Length > 0 && txtKValue.Text.Length > 0)
+            {
+                int input = Convert.ToInt32(txtInput.Text);
+                int kValue = Convert.ToInt32(txtKValue.Text);
+
+                double probability = getProbability(kValue);
+                result = isPrime(input, kValue);
+
+                if (result == true)
+                {
+                    txtOutput.Text = "Yes with a probability of " + probability.ToString() + ".";
+                }
+                else
+                {
+                    txtOutput.Text = "No.";
+                }
+            }
+            else
+            {
+                txtOutput.Text = "ERROR: Please fill all fields.";
+            }
+        }
+
+        private bool isPrime(int input, int kValue)
+        {
+            if (input == 1)
+            {
+                return true;
+            }
+            
+            // There are only up to (input - 2) random numbers that we're interested in using. Thus, 
+            // if kValue > (input - 2), we need to limit kValue to (input - 2).
+            if (kValue > (input - 2))
+            {
+                kValue = input - 2; 
+            }
+
+            // Initialized to false for case when input == 0.
+            bool isPrime = false;
+
+            for (int i = 0; i < kValue; i++)
+            {
+                int a = getRand(input);
+                int result = modularExponentiation(a, input - 1, input);
+
+                if (result == 1)
+                {
+                    isPrime = true;
+                }
+                else
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            // Clear all values from the HashSet that keeps track of the random numbers used.
+            // A new set should be generated each time "Solve" is clicked.
+            set.Clear();
+            return isPrime;
+        }
+
+        private int modularExponentiation(int x, int y, int original)
+        {
+            int newY = y;
+
+            if (y == 0)
+            {
+                return 1;
+            }
+            else if (y % 2 != 0)
+            {
+                newY -= 1;
+            }
+
+            // Recursive call.
+            int result = modularExponentiation(x, newY / 2, original);
+            int returnValue = (int)Math.Pow(result, 2);
+
+            // If even.
+            if (y % 2 != 0)
+            {
+                returnValue *= x;
+            }
+
+            // If mod is needed.
+            if (returnValue >= original)
+            {
+                returnValue = returnValue % original;
+            }
+
+            return returnValue;
+        }
+
+        // MARK: - HELPER METHODS
+        private int getRand(int upperBound)
+        {
+            int randNum = rand.Next(2, upperBound);
+
+            // Checks to see if the number has already 
+            // been used as a random number.
+            if (set.Contains(randNum))
+            {
+                randNum = getRand(upperBound);
+            }
+
+            // Adds the random number to the HashSet after being
+            // used in the computation.
+            set.Add(randNum);
+            return randNum;
+        }
+
+        private double getProbability(int kValue)
+        {
+            // This is the probability formula given to us 
+            // in the book, implemented in code.
+            return 1 - (1 / (Math.Pow(2, kValue)));
+        }
+    }
+}
